847fa65fd4aa9ac3f5228a5aed9f4a2b42eb68eb
john morris <fennymorris@live.co.uk>
2023-11-02 22:27:23 +0000

    return a GET list of json response, by creating a list of students which is returned as list json response

--- src/main/java/com/techlover/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/techlover/springbootrestapi/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.techlover.springbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 //    http://localhost:8080/student
@@ -15,5 +18,17 @@ public class StudentController {
                 "Morris"
         );
         return student;
+    }
+    // this returns a list of students as a json
+//    http://localhost:8080/students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Anna", "Morris"));
+        students.add(new Student(1, "Naomi", "Morris"));
+        students.add(new Student(1, "Peter", "Morris"));
+        students.add(new Student(1, "Bai", "Yu"));
+        students.add(new Student(1, "Stanky", "Nun"));
+        return students;
     }
 }
